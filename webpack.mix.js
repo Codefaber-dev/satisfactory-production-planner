6319c65038bcdd21964b0f32486513c266d5e12c
Jeremy Bloomstrom <jeremy.bloomstrom@gmail.com>
2021-07-16 22:36:20 -0800

    Install Jetstream

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,14 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+mix.js('resources/js/app.js', 'public/js').vue()
     .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+        require('postcss-import'),
+        require('tailwindcss'),
+    ])
+    .webpackConfig(require('./webpack.config'))
+    .browserSync('https://satis-pp.test');
+
+if (mix.inProduction()) {
+    mix.version();
+}
